5476c880e9fb73eabd3760dc5e1d4399683e5e3d
chaoranchen <chen4393@umn.com>
2017-04-04 12:09:54 -0500

    Ferry class

---
+++ src/Ferry.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+ * Created by chen4393 on 4/4/17.
+ */
+public class Ferry {
+
+    private ArrayList<Passenger> list = new ArrayList<Passenger>(60);
+
+    public boolean addPassenger(Passenger p) {
+        if (!isFull()) {
+            list.add(p);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Passenger[] removePassengersAtIsland(int island) {
+        ArrayList<Passenger> tempList = new ArrayList<Passenger>();
+        for (int i = 0; i < list.size(); i++) {
+            Passenger tempPassenger = list.get(i);
+            if (tempPassenger.getDropoffIsland() == island) {
+                list.remove(tempPassenger);
+                tempList.add(tempPassenger);
+            }
+        }
+        Passenger[] result = new Passenger[tempList.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = tempList.get(i);
+        }
+        return result;
+    }
+
+    public boolean isFull() {
+        return list.size() >= 60;
+    }
+}
---
+++ src/FerryEvent.java
@@ -0,0 +1,8 @@
+/**
+ * Created by chen4393 on 4/4/17.
+ */
+public class FerryEvent {
+
+    private int island;
+
+}
